702ef82e2e8de66702351eb768f5110d6e7d6710
Max Glen <xamnelg@gmail.com>
2025-11-18 19:34:52 -0500

    modified:   Hamiltonian_auto.jl

--- Hamiltonian_auto.jl
+++ Hamiltonian_auto.jl
@@ -1,15 +1,17 @@
 using ITensors, ITensorMPS
+using Plots
 
-m = 1
+m = 2
 w = 1
-N = 10
+N = 20
 F = 1
 C = 3
 J = 1
+L = 10000
 sites = siteinds("S=1/2", (N*F*C))
 
-function l(n, c)
-    return ((n-1)*F*C) + c - 1
+function l(n, f, c)
+    return ((n-1)*F*C) + (C*(f -1)) + c - 1
 end
 
 function addOp!(arr, op, site)
@@ -22,32 +24,55 @@ function addArr!(arr1, arr2)
     arr1 = append!(arr1, arr2[2:end])
 end
 
-function QijN(n, i, j, coeff)
-    coeff *= 1/sqrt(2)
+function product(A, B)
     ret = []
-    push!(ret, coeff)
-    addOp!(ret, "S+", l(n, i) + 1)
-    addArr!(ret, BigLambda(n, i, j))
-    addOp!(ret, "S-", l(n, j) + 1)
-    mpo = AutoMPO()
-    mpo += tuple(ret...)
-    return MPO(mpo, sites)
+    for a in A
+        for b in B
+            push!(ret, combine(a, b))
+        end
+    end
+    return ret
+end
+
+function combine(a, b)
+    coeff = a[1] * b[1]
+    ops = Any[a[2:end]..., b[2:end]...]
+
+    return tuple(coeff, ops...)
+end
+
+function QijN(n, i, j, coeff)
+    ret = []
+    for f=1: F
+        coeff = 1/sqrt(2)
+        temp = []
+        push!(temp, coeff)
+        addOp!(temp, "S+", l(n, f, i) + 1)
+        addArr!(temp, BigLambda(n, f, i, j))
+        addOp!(temp, "S-", l(n, f, j) + 1)
+        push!(ret, tuple(temp...))
+    end
+
+    return ret
 end
 
 function QiiN(n, i, coeff)
-    coeff *= (1/(2*sqrt(2i*(i -1))))
-    mpo = AutoMPO()
-    for c=1 : i - 1
-        mpo += coeff, "Z", l(n, c) + 1
-        mpo += -1 * coeff, "Z", l(n, i) + 1
+    coeff = 1
+    ret = []
+    for f=1: F
+        coeff *= (1/(2*sqrt(2i*(i -1))))
+        for c=1 : i - 1
+            push!(ret, (coeff, "Z", l(n, f, c) + 1))
+            push!(ret, (-1 * coeff, "Z", l(n, f, i) + 1))
+        end
     end
-    return MPO(mpo, sites)
+    return ret
 end
 
-function BigLambda(n, i, j)
+function BigLambda(n, f, i, j)
     ret = []
     coeff = 1
-    for k=l(n, j) : l(n, i) - 1
+    for k=l(n, f, j) : l(n, f, i) - 1
         coeff *= -1
         addOp!(ret, "Z", k + 1)
     end
@@ -56,61 +81,127 @@ function BigLambda(n, i, j)
 end 
 
 function QiNQiM(n, m, coeff)
-    ret = MPO()
-    flag = true
+    ret = []
     for i=2 : C 
         for j=1: i - 1
             A = QijN(n, j, i, coeff)
             B = QijN(m, i, j, coeff)
-            mpo1 = contract(A, B; alg="naive", truncate=false)
-            mpo1 = apply(A, B; alg="naive", truncate=false)
-            if flag
-                ret = mpo1
-                flag = false
-            else 
-                +(ret, mpo1)
-            end
+            append!(ret, product(A, B))
             A = QijN(n, i, j, coeff)
             B = QijN(m, j, i, coeff)
-            mpo2 = contract(A, B; alg="naive", truncate=false)
-            mpo2 = apply(A, B; alg="naive", truncate=false)
-            +(ret, mpo2)
+            append!(ret, product(A, B))
         end
     end
     for i=2 : C
         A = QiiN(n, i, coeff)
         B = QiiN(m, i, coeff)
-        mpo = contract(A, B; alg="naive", truncate=false)
-        mpo = apply(A, B; alg="naive", truncate=false)
-        +(ret, mpo)
+        append!(ret, product(A, B))
     end
     return ret
 end
 
-let 
+function plot_observables(psi, N, F, C)
+    # Calculate expectation value of Sz at every site
+    sz = expect(psi, "Sz") 
+    
+    # Initialize a plot
+    p = plot(title="Local Magnetization <Sz>", xlabel="Lattice Site (n)", ylabel="<Sz>")
+    
+    # Loop through "Colors" (C) and plot them as separate series
+    for c_idx = 1:C
+        # Extract only the indices corresponding to this color
+        # We reconstruct the linear index: l(n, c) + 1
+        indices = [((n-1)*F*C) + c_idx - 1 + 1 for n in 1:N]
+        
+        # Plot this subset
+        plot!(p, 1:N, sz[indices], label="Color $c_idx", marker=:circle)
+    end
+    display(p)
+end
 
-    #Mass
-    function Mass()
-        Mass = AutoMPO()
-        for n=1: N 
-            i = isodd(n) ? -1 : 1
-            coeff = 0.5*m*i
-            for c=1: C
-                Mass += coeff, "Z", l(n, c) + 1
-                Mass += coeff, "Id", l(n, c) + 1
+function plot_correlations(psi)
+    # specific operators
+    M = correlation_matrix(psi, "Sz", "Sz")
+    
+    # Plot as a heatmap
+    hm = heatmap(M, 
+        title="Spin-Spin Correlation <Sz_i Sz_j>", 
+        xlabel="Site j", 
+        ylabel="Site i",
+        color=:viridis,
+        aspect_ratio=:equal
+    )
+    display(hm)
+end
+
+function plot_entanglement(psi, N_total)
+    entropies = Float64[]
+    
+    # Orthogonalize to the first site to start
+    orthogonalize!(psi, 1)
+    
+    for b in 1:(N_total-1)
+        # Singular Value Decomposition at bond b
+        # ITensor manages the orthogonality center automatically if you move sequentially
+        orthogonalize!(psi, b)
+        
+        # Get the singular values (spectrum of the density matrix)
+        _, S, _ = svd(psi[b], (linkinds(psi, b-1)..., siteinds(psi, b)...))
+        
+        # Calculate Von Neumann Entropy: - sum(p * log(p))
+        Sv = 0.0
+        for n in 1:dim(S, 1)
+            p = S[n, n]^2
+            if p > 1e-12 # Avoid log(0)
+                Sv -= p * log(p)
             end
         end
-        return MPO(Mass, sites)
-    end 
+        push!(entropies, Sv)
+    end
+
+    p = plot(entropies, 
+        title="Entanglement Entropy (Von Neumann)", 
+        xlabel="Bond Cut", 
+        ylabel="Entropy S_vn", 
+        legend=false,
+        fill=(0, 0.2, :blue) # Fill area under curve
+    )
+    display(p)
+end
+
+function number_op(psi, w, n)
+    num = AutoMPO()
+
+    for c=1: C
+        num += (w/2), "Sz", l(2*n, 1, c) + 1
+        num += (w/2), "Sz", l((2*n)-1, 1, c) + 1
+    end
+
+    return inner(psi', MPO(num, siteinds(psi)), psi)
+end
+
+let 
+    #Mass
+    Mass = AutoMPO()
+    for n=1: N 
+        i = isodd(n) ? -1 : 1
+        coeff = 0.5*m*i
+        for f=1: F
+            for c=1: C
+                Mass += coeff, "Z", l(n, f, c) + 1
+                Mass += coeff, "Id", l(n, f, c) + 1
+            end
+        end
+    end
 
     #Hopping
-    function Hopping()
-        Hopping = AutoMPO()
-        for n=1: N-1
+    Hopping = AutoMPO()
+    for n=1: N-1
+        for f=1: F
             for c=1: C
                 for i=1 : 2
-                    s1 = l(n+1, c)
-                    s2 = l(n, c)
+                    s1 = l(n+1, f, c)
+                    s2 = l(n, f, c)
                     if i==1
                         temp = []
                         coeff = 1
@@ -137,39 +228,38 @@ let
                 end
             end
         end
-        return MPO(Hopping, sites)
     end
 
     #Electric
-    function Electric()
-        Electric = MPO()
-        flag = true
-        for n=1 : N - 1
-            coeff = N - n
-            if flag
-                Electric = QiNQiM(n, n, J * coeff)
-                flag = false
-            else
-                +(Electric, QiNQiM(n, n, J * coeff))
-            end
+    Electric = AutoMPO()
+    for n=1 : N - 1
+        coeff = (N) - n
+        for i in QiNQiM(n, n, J * coeff)
+            Electric += i
         end
-        for n=1 : N - 2
-            for m=n+1 : N - 1
-                coeff = N - m
-                +(Electric, QiNQiM(n, m, 2 * J * coeff))
-            end
+    end
+    for n=1 : N - 2
+        for m=n+1 : N - 1
+            coeff = (N) - m 
+            for i in QiNQiM(n, m, 2 * J * coeff)
+                Electric += i
+            end 
         end
-        return Electric
     end
 
-    println(Hopping())
-    println("------------------")
-    println(Mass())
-    println("------------------")
-    println(Electric())
-    H = +(Hopping() + Mass() + Electric())
+    #Flux
+    Flux = AutoMPO()
+    for n1=1: N
+        for n2=1: N
+            for i in QiNQiM(n1, n2, L)
+                Flux += i
+            end
+        end
+    end
 
-    nsweeps = 5 # number of sweeps is 5
+    H = MPO(Hopping + Mass + Electric + Flux, sites)
+
+    nsweeps = 500 # number of sweeps is 5
     maxdim = [10,20,100,100,200] # gradually increase states kept
     cutoff = [1E-10] # desired truncation error
 
@@ -178,5 +268,19 @@ let
     energy,psi = dmrg(H,psi0;nsweeps,maxdim,cutoff)
 
     print(energy)
+
+    #plot_observables(psi, N, F, C)
+
+    #plot_correlations(psi)
+
+    #plot_entanglement(psi, N*F*C)
+    ret = []
+    for n=1: div(N,2)
+        push!(ret, number_op(psi0, w, n))
+    end
+
+    p = plot(title="Number operator vaules for Random and Minimized states", xlabel="Site", ylabel="Number operator value")
+    plot!(p, ret, label="Minimized")
+    display(p)
 
 end
