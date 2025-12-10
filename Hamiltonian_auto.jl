38287af5b68c8ab1be4909271a87fb9d935537ec
Max Glen <xamnelg@gmail.com>
2025-12-10 00:18:25 -0500

    modified:   Hamiltonian_auto.jl Changed electric component to calculate J after construction of MPO for efficiency. Added Chiral condensate code. Added net charge calc for verification.

--- Hamiltonian_auto.jl
+++ Hamiltonian_auto.jl
@@ -6,8 +6,8 @@ using ProgressMeter
 using LinearAlgebra
 using JLD2
 
-BLAS.set_num_threads(1)
-ITensors.Strided.set_num_threads(1) # Disable block-sparse multithreading
+BLAS.set_num_threads(4)
+ITensors.Strided.set_num_threads(4) # Disable block-sparse multithreading
 
 struct ModelParams
     N::Int
@@ -112,6 +112,175 @@ function QiNQiM(n, o, p::ModelParams)
         append!(ret, multiply_terms(QiiN(n, i, p), QiiN(o, i, p)))
     end
     return ret
+end
+
+function construct_mass_op(sites, p::ModelParams)
+    os = OpSum()
+    for n in 1:p.N
+        sign_val = isodd(n) ? -1.0 : 1.0
+        # Note: We do NOT multiply by mass here, we do it in the loop later
+        coeff = sign_val 
+        for f in 1:p.F
+            for c in 1:p.C
+                idx = l(n, f, c, p) + 1
+                os += coeff, "Sz", idx
+                os += coeff, "Id", idx
+            end
+        end
+    end
+    return MPO(os, sites)
+end
+
+function construct_hopping_op(sites, p::ModelParams)
+    os = OpSum()
+    for n in 1:(p.N - 1)
+        for f in 1:p.F
+            for c in 1:p.C
+                s1 = l(n+1, f, c, p)
+                s2 = l(n, f, c, p)
+                
+                # Term 1: S+(s1) ... S-(s2)
+                c1 = 1.0
+                ops1 = Any[]
+                push!(ops1, "S+", s1 + 1)
+                for k in s2:(s1-1)
+                    c1 *= -2.0
+                    push!(ops1, "Sz", k + 1)
+                end
+                push!(ops1, "S-", s2 + 1)
+                # Combine coeff and ops into one tuple
+                os += (c1, ops1...)
+                
+                # Term 2: S+(s2) ... S-(s1)
+                c2 = 1.0
+                ops2 = Any[]
+                push!(ops2, "S+", s2 + 1)
+                for k in s2:(s1-1)
+                    c2 *= -2.0
+                    push!(ops2, "Sz", k + 1)
+                end
+                push!(ops2, "S-", s1 + 1)
+                os += (c2, ops2...)
+            end
+        end
+    end
+    return MPO(os, sites)
+end
+
+function construct_electric_op(sites, p::ModelParams)
+    os = OpSum()
+
+    # Internal helper to add terms to local os
+    function add_terms_locally!(terms, factor)
+        for t in terms
+            final_coeff = t.coeff * factor
+            if abs(final_coeff) > 1e-14
+                args = Any[final_coeff]
+                for op in t.ops
+                    push!(args, op.name, op.site)
+                end
+                os += Tuple(args)
+            end
+        end
+    end
+
+    for n in 1:(p.N - 1)
+        coeff = Float64(p.N - n)
+        terms = QiNQiM(n, n, p)
+        add_terms_locally!(terms, coeff)
+    end
+    for n in 1:(p.N - 2)
+        for o in (n + 1):(p.N - 1)
+            coeff = Float64(p.N - o)
+            terms = QiNQiM(n, o, p)
+            add_terms_locally!(terms, coeff * 2)
+        end
+    end
+    return MPO(os, sites)
+end
+
+function construct_flux_op(sites, p::ModelParams)
+    os = OpSum()
+
+    function add_terms_locally!(terms, factor)
+        for t in terms
+            final_coeff = t.coeff * factor
+            if abs(final_coeff) > 1e-14
+                args = Any[final_coeff]
+                for op in t.ops
+                    push!(args, op.name, op.site)
+                end
+                os += Tuple(args)
+            end
+        end
+    end
+
+    for n1 in 1:p.N
+        for n2 in 1:p.N
+            terms = QiNQiM(n1, n2, p)
+            add_terms_locally!(terms, 1)
+        end
+    end
+    return MPO(os, sites)
+end
+
+function construct_hamiltonian(p, s)
+    params = p
+    sites = s
+
+    m = params.m0/params.g
+    w = 1.0 / (2 * params.a * params.g)
+    J = (params.a * params.g) / 2.0
+
+    #Mass
+    Mass = construct_mass_op(sites, params) * m
+
+    #Hopping
+    
+    Hopping = construct_hopping_op(sites, params) * w
+
+    #Electric
+    
+    Electric = construct_electric_op(sites, params) * J
+
+    #Flux
+    if p.L != 0.0
+        Flux = construct_flux_op(sites, params) * L
+        return Mass + Hopping + Electric + Flux
+    end
+
+    return Mass + Hopping + Electric
+end
+
+function measure_total_flux_squared(psi, p)
+    if p.L == 0.0
+        factor = 1.0
+    else
+        factor = p.L
+    end
+    os = OpSum()
+    for n1 in 1:p.N
+        for n2 in 1:p.N
+            terms = QiNQiM(n1, n2, p) 
+            for t in terms
+                final_coeff = t.coeff * factor
+                if abs(final_coeff) > 1e-14
+                    args = Any[final_coeff]
+                    for op in t.ops
+                        push!(args, op.name, op.site)
+                    end
+                    os += Tuple(args)
+                end
+            end
+        end
+    end
+    
+    flux_squared_op = MPO(os, siteinds(psi))
+    
+    val = inner(psi', flux_squared_op, psi)
+    
+    println("Total Outgoing Flux Squared <Q^2>: $val")
+    return val
 end
 
 function plot_observables(psi, N, F, C)
@@ -252,150 +421,12 @@ function phase_diagram(steps)
     display(hm)
 end
 
-function construct_mass_op(sites, p::ModelParams)
-    os = OpSum()
-    for n in 1:p.N
-        sign_val = isodd(n) ? -1.0 : 1.0
-        # Note: We do NOT multiply by mass here, we do it in the loop later
-        coeff = sign_val 
-        for f in 1:p.F
-            for c in 1:p.C
-                idx = l(n, f, c, p) + 1
-                os += coeff, "Sz", idx
-                os += coeff, "Id", idx
-            end
-        end
-    end
-    return MPO(os, sites)
-end
-
-function construct_hopping_op(sites, p::ModelParams)
-    os = OpSum()
-    for n in 1:(p.N - 1)
-        for f in 1:p.F
-            for c in 1:p.C
-                s1 = l(n+1, f, c, p)
-                s2 = l(n, f, c, p)
-                
-                # Term 1: S+(s1) ... S-(s2)
-                c1 = 1.0
-                ops1 = Any[]
-                push!(ops1, "S+", s1 + 1)
-                for k in s2:(s1-1)
-                    c1 *= -2.0
-                    push!(ops1, "Sz", k + 1)
-                end
-                push!(ops1, "S-", s2 + 1)
-                # Combine coeff and ops into one tuple
-                os += (c1, ops1...)
-                
-                # Term 2: S+(s2) ... S-(s1)
-                c2 = 1.0
-                ops2 = Any[]
-                push!(ops2, "S+", s2 + 1)
-                for k in s2:(s1-1)
-                    c2 *= -2.0
-                    push!(ops2, "Sz", k + 1)
-                end
-                push!(ops2, "S-", s1 + 1)
-                os += (c2, ops2...)
-            end
-        end
-    end
-    return MPO(os, sites)
-end
-
-function construct_electric_op(sites, p::ModelParams, J::Float64)
-    os = OpSum()
-
-    # Internal helper to add terms to local os
-    function add_terms_locally!(terms, factor)
-        for t in terms
-            final_coeff = t.coeff * factor
-            if abs(final_coeff) > 1e-14
-                args = Any[final_coeff]
-                for op in t.ops
-                    push!(args, op.name, op.site)
-                end
-                os += Tuple(args)
-            end
-        end
-    end
-
-    for n in 1:(p.N - 1)
-        coeff = Float64(p.N - n)
-        terms = QiNQiM(n, n, p)
-        add_terms_locally!(terms, coeff * J)
-    end
-    for n in 1:(p.N - 2)
-        for o in (n + 1):(p.N - 1)
-            coeff = Float64(p.N - o)
-            terms = QiNQiM(n, o, p)
-            add_terms_locally!(terms, coeff * 2 * J)
-        end
-    end
-    return MPO(os, sites)
-end
-
-function construct_flux_op(sites, p::ModelParams)
-    os = OpSum()
-
-    function add_terms_locally!(terms, factor)
-        for t in terms
-            final_coeff = t.coeff * factor
-            if abs(final_coeff) > 1e-14
-                args = Any[final_coeff]
-                for op in t.ops
-                    push!(args, op.name, op.site)
-                end
-                os += Tuple(args)
-            end
-        end
-    end
-
-    for n1 in 1:p.N
-        for n2 in 1:p.N
-            terms = QiNQiM(n1, n2, p)
-            add_terms_locally!(terms, p.L)
-        end
-    end
-    return MPO(os, sites)
-end
-
-function construct_hamiltonian(p, s)
-    params = p
-    sites = s
-
-    m = params.m0/params.g
-    w = 1.0 / (2 * params.a * params.g)
-    J = (params.a * params.g) / 2.0
-
-    #Mass
-    Mass = construct_mass_op(sites, params) * m
-
-    #Hopping
-    
-    Hopping = construct_hopping_op(sites, params) * w
-
-    #Electric
-    
-    Electric = construct_electric_op(sites, params, J)
-
-    #Flux
-    if p.L != 0.0
-        Flux = construct_flux_op(sites, params)
-        return Mass + Hopping + Electric + Flux
-    end
-
-    return Mass + Hopping + Electric
-end
-
 function phase_diagram_mn(steps)
     # Define Base Parameters
     F, C = 1, 3
     a_val, g_val, m0_val, L_val = 1.0, 1.0, 1.0, 0.0
     
-    mass_limit = 20.0
+    mass_limit = 8.0
     mass_vals = collect(range(-mass_limit, mass_limit, length=steps*4))
     n_vals = [i for i in 1:steps if iseven(i)]
     
@@ -417,11 +448,11 @@ function phase_diagram_mn(steps)
 
         m_op = construct_mass_op(local_sites, params)
         h_op  = construct_hopping_op(local_sites, params)
-        e_op = construct_electric_op(local_sites, params, J)
+        e_op = construct_electric_op(local_sites, params)
         #f_op = construct_flux_op(local_sites, params)
 
 
-        H_fixed = (w * h_op) + e_op
+        H_fixed = (w * h_op) + e_op * J
         
         Threads.@threads :dynamic for j in 1:length(mass_vals)
             mass_step = mass_vals[j]
@@ -442,7 +473,6 @@ function phase_diagram_mn(steps)
 
     # ... loops finished ...
 
-    # Trim data for saving (optional: you can save the raw 'results' instead if you prefer)
     threshold = 1000.0
     results_to_save = copy(results)
     results[results .> threshold] .= NaN
@@ -470,11 +500,105 @@ function phase_diagram_mn(steps)
     return results
 end
 
+function measure_chiral_condensate(psi, p)
+    sz_exp = expect(psi, "Sz")
+    
+    total_val = 0.0
+    for n in 1:p.N
+        parity = isodd(n) ? -1.0 : 1.0
+        
+        for f in 1:p.F
+            for c in 1:p.C
+                idx = l(n, f, c, p) + 1
+                total_val += parity * sz_exp[idx]
+            end
+        end
+    end
+    
+    return abs(total_val / p.N) 
+end
+
+function phase_diagram_condensate(steps, p)
+    
+    M_condensate = zeros(steps, steps)
+    M_charge = zeros(steps, steps)
+    
+    mass_range = range(-2.0, 2.0, length=steps)
+    g_range = range(0.1, 1.0, length=steps) # Avoid g=0 to prevent division by zero
+    sites = siteinds("S=1/2", p.N * p.F * p.C, conserve_qns=true)
+    
+    println("Starting Phase Diagram Scan...")
+
+    m_op = construct_mass_op(sites, p)
+    h_op  = construct_hopping_op(sites, p)
+    e_op = construct_electric_op(sites, p)
+    #f_op = construct_flux_op(sites, params)
+
+    p_meter = Progress(steps * steps, desc="Simulation Progress: ", barglyphs=BarGlyphs("[=> ]"))
+
+    
+    Threads.@threads :dynamic for i in 1:steps
+        w = 1.0 / (2 * p.a * g_range[i])
+        J = (p.a * g_range[i]) / 2.0
+       
+        H_fixed = (h_op * w) + (e_op * J)
+        for j in 1:steps
+            m = mass_range[j] / g_range[i]
+            H = (m_op * m) + H_fixed
+            
+            DMRG Parameters
+            nsweeps = 10
+            maxdim = [10, 20, 50, 100, 200, 400, 1000]
+            noise = [1E-4, 1E-5, 0.0]
+            cutoff = [1E-6, 1E-8, 1E-12]
+            
+            state_array = [isodd(div(x-1, p.F*p.C) + 1) ? "Up" : "Dn" for x in 1:(p.N*p.F*p.C)]
+            psi_init = random_mps(sites, state_array)
+
+            energy, psi0 = dmrg(H, psi_init; nsweeps, maxdim, cutoff, noise, outputlevel=0)
+
+            M_charge[j, i] = measure_total_flux_squared(psi0, p) 
+
+            #Measure Condensate
+            val = measure_chiral_condensate(psi0, p)
+            
+            M_condensate[j, i] = val
+            next!(p_meter)
+        end
+    end
+
+    # Plotting
+    plot = heatmap(mass_range, g_range, M_condensate', 
+        title = "Chiral Condensate Phase Diagram",
+        xlabel = "Mass Parameter (m)",
+        ylabel = "Coupling Constant (g)",
+        color = :viridis,
+        aspect_ratio = :auto # Allows the plot to stretch to fill the window
+    )
+    filename = "chiral_condensate_PD_N" *  string(p.N) * "_C" * string(p.C) * "_F" * string(p.F)
+
+    jldsave(filename * ".jld2"; 
+    M_condensate = M_condensate, 
+    M_charge = M_charge
+    )
+    savefig(filename * ".png")
+    plot2 = heatmap(mass_range, g_range, M_charge', 
+    title = "Chiral Condensate Phase Diagram (charge)",
+    xlabel = "Mass Parameter (m)",
+    ylabel = "Coupling Constant (g)",
+    color = :viridis,
+    aspect_ratio = :auto # Allows the plot to stretch to fill the window
+    )
+    savefig(filename * "_charge" * ".png")
+    display(plot)
+    return M_condensate
+end
+
 function calc_energy_gap(p::ModelParams, sites, H, show_output::Bool)
     nsweeps = 15
-    maxdim = [10, 20, 100, 200, 400, 800, 1000, 2000]
-    cutoff = [1E-6, 1E-8, 1E-12]
-    noise = [1E-4, 1E-5, 1E-6, 0.0]
+    maxdim = [10, 20, 50, 100, 200, 400, 800, 1000, 2000, 4000]
+    cutoff = [1E-6, 1E-8, 1E-10, 1E-12]
+    noise = [1E-4, 1E-5, 1E-6, 1E-8, 0.0]
 
     total_sites = length(sites)
     state_array = [isodd(div(x-1, p.F * p.C) + 1) ? "Up" : "Dn" for x in 1:total_sites]
@@ -515,9 +639,11 @@ function calc_energy_gap(p::ModelParams, sites, H, show_output::Bool)
 end
 
 let 
-    phase_diagram_mn(10)
 
-    # params = ModelParams(10, 1, 3, 1.0, 1.0, 10.0, 0.0)
+    params = ModelParams(10, 1, 3, 1.0, 1.0, 20.0, 0)
+    # phase_diagram_mn(16)
+    # phase_diagram(20)
+    phase_diagram_condensate(2, params)
     # sites = siteinds("S=1/2", params.N * params.F * params.C, conserve_qns=true)
     # H = construct_hamiltonian(params, sites)
     # calc_energy_gap(params, sites, H, true)
